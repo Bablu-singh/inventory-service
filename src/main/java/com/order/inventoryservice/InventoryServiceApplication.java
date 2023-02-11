d66b2eaf3d0f58b2b6094eb1593e7ecd6c3a3d0d
bablu <bs903944@gmail.com>
2023-02-11 11:27:52 +0530

    initial commit

---
+++ src/main/java/com/order/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,25 @@
+package com.order.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class InventoryServiceApplication {
+
+	@GetMapping("/test")
+	public ResponseEntity<String> test(){
+		return ResponseEntity.ok("Inventory");
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
